63a31fb85d313fc024c8e51886a8c064424a472c
bitwater <luosanshui@foxmail.com>
2021-02-04 13:12:47 +0800

    add build/

---
+++ build/precache-manifest.b3765ac429828c8aeb7ab0ed001316f0.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a31d6bbbdf7f2a8e6a4644dd7f133c50",
+    "url": "/index.html"
+  },
+  {
+    "revision": "44c2bf40d877172baea7",
+    "url": "/static/js/2.9cf41cf2.chunk.js"
+  },
+  {
+    "revision": "48dc2a0f18dfdbbcdcf81b94d8a3e1da",
+    "url": "/static/js/2.9cf41cf2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f470179935744be569a1",
+    "url": "/static/js/main.361f102d.chunk.js"
+  },
+  {
+    "revision": "d7b5075ecf70ba60e7e7",
+    "url": "/static/js/runtime-main.9cb2a799.js"
+  },
+  {
+    "revision": "73dabb371ddce7c640334fe0904dcddf",
+    "url": "/static/media/eth.73dabb37.png"
+  },
+  {
+    "revision": "5827780d34d7c141d5e763fd7b596313",
+    "url": "/static/media/logo.5827780d.svg"
+  },
+  {
+    "revision": "edb44e56409a7af705c227bd272fac47",
+    "url": "/static/media/logo_white.edb44e56.svg"
+  },
+  {
+    "revision": "9914390fc6b153e39de552594e0b1d3a",
+    "url": "/static/media/wordmark_white.9914390f.svg"
+  }
+]);
